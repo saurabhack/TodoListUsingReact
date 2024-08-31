430d3186e6bf4876466cf514c8491ca3a18d989c
saurabhack <kamanesaurabh68@gmail.com>
2024-08-31 16:23:23 +0530

    complete todo list in react

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -6,6 +6,28 @@ function Todo(){
         let newTodo= prompt("add your todo")
         setTodo([...todo,[newTodo]])
     }
+    function DeleteItem(id){
+        const updatedTodoList = todo.filter((val,i)=>{
+            return i !== id;
+        })
+
+        console.log(updatedTodoList)
+
+        setTodo(updatedTodoList)
+
+    }
+    function Update(id){
+        let updatedTodo=prompt("update your todo =")
+        let manage=todo.forEach((val,i)=>{
+            return i==id
+        })
+        let ask=todo.filter((val,i)=>{
+            return i!=id;
+        })
+        manage=updatedTodo
+        let newTodo=[...ask,[manage]]
+        setTodo(newTodo)
+    }
     return(
         <>
         <div className="h-[100vh] w-[100vw] border border-red-800">
@@ -15,7 +37,11 @@ function Todo(){
             <ul>
                 {
                     todo.map((val,i)=>{
-                        return <li className=" border border-red-800 m-4 p-3 rounded-md flex justify-center items-center gap-7" key={i} >{val} <button  className="bg-red-800 text-white p-3 m-1 rounded-md">delete</button> </li> 
+                        return <li className=" border border-red-800 m-4 p-3 rounded-md flex justify-center items-center gap-7" key={i} >{val} <button onClick={()=>{
+                            DeleteItem(i)
+                        }}  className="bg-red-800 text-white p-3 m-1 rounded-md">delete</button> <button onClick={()=>{
+                            Update(i)
+                        }}  className="bg-red-800 text-white p-3 m-1 rounded-md">Update</button> </li> 
                     })
                 }
             </ul>
